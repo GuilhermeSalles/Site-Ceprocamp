ed5d1ece1da0f1edbaab653bdf279cb94e5ed769
GuilhermeSalles <guibaltazarvs@gmail.com>
2021-07-15 21:21:52 -0300

    Pagina Filtros refeita.

--- Trabalho_Ceprocamp/js/filtro.js
+++ Trabalho_Ceprocamp/js/filtro.js
@@ -1,31 +1,38 @@
+ 
+  // Filtro portfólio
 
-(function($)
-{
-	
-	
-	jQuery(window).on('load', function() {
-		preloader();
-		
-		
-		if(jQuery('.gallery-outer .gallery-items').length >= 0){
-			jQuery('.gallery-outer .gallery-items').filterizr();
-		}
-		jQuery('#filter-list li').on("click", function(){
-			jQuery('#filter-list li').removeClass('activef');
-			jQuery(this).addClass('activef');
-		});
-	});
-	
-	
-	
-	
-	function preloader(){
-		jQuery(".preloaderimg").fadeOut();
-		jQuery(".preloader").delay(200).fadeOut("slow").delay(200, function(){
-			jQuery(this).remove();
-		});
-	}
-	
-	
-	
-})(jQuery);	
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+ 
