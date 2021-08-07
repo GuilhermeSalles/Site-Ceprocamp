1ebdce15d3313e83fdb27fd6a713b0d5d26fe8f8
GuilhermeSalles <guibaltazarvs@gmail.com>
2021-08-06 23:15:42 -0300

    Paginas cursos atualizadas na parte do texto

--- Trabalho_Ceprocamp/Curso/Português_e_matemática_para_concursos_públicos_e_enem.php
+++ Trabalho_Ceprocamp/Curso/Português_e_matemática_para_concursos_públicos_e_enem.php
@@ -3,7 +3,6 @@
 
     <!-- Inicio imagem do curso -->
 
-
     <br>
     <div class="container">
         <div class="row">
@@ -22,7 +21,6 @@
     <br>
 
     <!-- Fim imagem -->
-    <!-- <div class="jumbotron jumbotron-fluid" style="background-color: #28542d46;"> -->
     <!-- Começo que fala sobre o curso entre outras coisas -->
     <br>
     <div class="container">
@@ -39,14 +37,33 @@
                 </p>
             </div>
         </div>
+        
         <br>
         <br>
 
-        <div class="row">
-            
-        <?php include 'texto-inscrever.php' ?>
+        <div class="row"> 
+
+            <div class="col-sm-12 col-md-8 col-xs-8" data-anime="left">
+
+                <h2 style="font-weight: bold; color: rgb(75, 75, 75);">Como me inscrever</h2>
+                <hr class="imgR-small hr5r mt-0">
+
+                <p class="text-justify">Para se inscrever nos cursos oferecidos o candidato deverá ficar atento a
+                    publicação do Edital.
+                    Ele estabelece os critérios e procedimentos relativos ao ingresso de alunos, contendo todas as
+                    disposições e detalhes sobre os cursos oferecidos pelo CEPROCAMP.</p>
+
+                <br>
+
+                <h6 style="font-weight: bold; color: rgb(75, 75, 75);">Requisitos Gerais:</h6>
+                <ul class="b ml-5">
+                    <li>Morador de Campinas e da Região Metropolitana de Campinas.</li>
+                    <li>Possua seu próprio CPF.</li>
+                    <li>Possuir endereço eletrônico (e-mail) válido.</li>
+                </ul>
+ 
         
-        <h6 style="font-weight: bold; color: rgb(75, 75, 75);">Requisitos Específicos:</h6>
+                <h6 style="font-weight: bold; color: rgb(75, 75, 75);">Requisitos Específicos:</h6>
                 <ul class="b ml-5">
                     <li>Possuir 16 anos completos até o último dia da inscrição.</li>
                     <li>Possuir o pré-requisito exigido para cada curso, de forma a aproveitar totalmente a aprendizagem
