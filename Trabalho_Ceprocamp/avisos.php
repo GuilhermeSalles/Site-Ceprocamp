f7d075bd19063d5d5a4fad445d01f3376a3943f6
GuilhermeSalles <guibaltazarvs@gmail.com>
2021-08-10 22:01:53 -0300

    Avisos atualizados

--- Trabalho_Ceprocamp/avisos.php
+++ Trabalho_Ceprocamp/avisos.php
@@ -1,4 +1,4 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: #22115d56;">
         <div class="container">
             <div class="tab-content" id="nav-pills-content">
                 <div id="carouselAviso" class="carousel slide d-block" data-ride="carousel" data-interval="5000">
@@ -8,20 +8,42 @@
                         <div class="carousel-item active">
                             <div class="text-justify">
 
-                                <img src="img/fumec_2.png" class="img-fluid" alt="Imagem responsiva">
-
-                                <br>
-                                <br> 
-                                <br> 
-                                <br> 
-
+                                <img src="img/Avisos/Biblioteca.png" class="img-fluid" alt="Aviso Bilioteca">
+                                
                             </div>
                         </div>
+                        <div class="carousel-item">
+                            <div class="text-justify">
+                                
+                                <img src="img/Avisos/Secretaria.png" class="img-fluid" alt="Secretária">
+                                
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="text-justify">
+
+                                <img src="img/Avisos/Sobre NIT.png" class="img-fluid" alt="Aviso sobre Nit">
+                                
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="text-justify">
+
+                                <img src="img/Avisos/Aplicativo.png" class="img-fluid" alt="Aplicativo">
+                                
+                            </div>
+                        </div>
+
                     </div>
-                     
+                    
+                    <br> 
+                    <br>
+                    
                     <ol class="carousel-indicators">
                         <li data-target="#carouselAviso" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselAviso" data-slide-to="1"></li>
+                        <li data-target="#carouselAviso" data-slide-to="2"></li>
+                        <li data-target="#carouselAviso" data-slide-to="3"></li>
                     </ol> 
                 </div>
             </div>
