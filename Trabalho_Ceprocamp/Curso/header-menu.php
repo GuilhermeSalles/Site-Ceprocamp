a049555c7bb52001542414c89ce047df1ca5602b
GuilhermeSalles <guibaltazarvs@gmail.com>
2021-08-16 22:38:33 -0300

    Menus atualizados v.2

--- Trabalho_Ceprocamp/Curso/header-menu.php
+++ Trabalho_Ceprocamp/Curso/header-menu.php
@@ -5,29 +5,25 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="../img/logo.png" type="image/png">
+    <link rel="shortcut icon" href="img/logo.png" type="image/png">
     <!-- Scripts (jQuery não pode ser o slim que vem do Boostrap) -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
     <link rel="stylesheet" href="../CSS/Principal.css">
     <link rel="stylesheet" href="../CSS/cursos.css">
 
     <title>CEPROCAMP - Centro de Educação Profissional de Campinas |</title>
+
 </head>
 
 <body>
-    
+
     <!-- Logos antes do menu -->
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-sm-2 d-lg-block d-none ">
                 <img src="../img/logo_campinasP.png" class="img d-block">
@@ -42,8 +38,9 @@
             </div>
         </div>
     </div>
-    
     <!-- Menu -->
+
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" data-toggle="affix">
         <div class="container">
             <div class="justify-content-left">
@@ -53,31 +50,41 @@
                     </span>
                 </button>
             </div>
-            <div class="collapse navbar-collapse" id="navbarSite">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <a class="navbar-brand h1 mb-1 justify-content-right" href="../index">Inicio</a>
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="navDrop">
-                                    Unidades
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="../cambara">Cambará</a>
-                                    <a class="dropdown-item" href="../campoGrande">Campo Grande</a>
-                                    <a class="dropdown-item" href="../casi">CASI</a>
-                                    <a class="dropdown-item" href="../index">Centro</a>
-                                    <a class="dropdown-item" href="../joseAlves">José Alves</a>
-                                    <a class="dropdown-item" href="../myrian">Myrian</a>
-                                    <a class="dropdown-item" href="../ouroVerde">Ouro Verde</a>
-                                </div>
-                            </li>
+
+                <ul class="navbar-nav">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Unidades
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+
+                            <a class="dropdown-item" href="../index">Centro</a>
+                            <a class="dropdown-item" href="../joseAlves">José Alves</a>
+                            <a class="dropdown-item" href="../ouroVerde">Ouro Verde</a>
+                            <a class="dropdown-item" href="../cambara">Cambará</a>
+                            <a class="dropdown-item" href="../campoGrande">Campo Grande</a>
+                            <a class="dropdown-item" href="../casi">CASI</a>
+                            <a class="dropdown-item" href="../myrian">Myrian</a>
+                            <a class="dropdown-item" href="../Parceiras">Parceiras</a>
+
+                            <!--
+                            <li class="dropdown-submenu">
+                                <a class="dropdown-item dropdown-toggle" href="#">sub menu</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Submenu action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another submenu action</a></li>
+                                </ul>
+                            </li> -->
+
                         </ul>
                     </li>
 
+
                     <li class="nav-item">
-                        <a class="nav-link" href="HTML/sedes/Unidades Parceiras/PaginaUniParceiras">Unidades
-                            Parceiras</a>
+                        <a class="nav-link" href="../CursosQualificacao">Cursos</a>
                     </li>
 
                     <li class="nav-item">
@@ -88,23 +95,15 @@
                                 </a>
                                 <div class="dropdown-menu">
 
-                                    <a class="dropdown-item" href="../Doc/calendario_homologado_2021.pdf"
-                                        target="_blank">Calendário</a>
-                                    <a class="dropdown-item" href="https://fumec.sp.gov.br/eja/eja-profissoes"
-                                        target="_blank">EJA Profissões</a>
-                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/matriz%20curricular "
-                                        target="_blank">Matrizes dos cursos</a>
+                                    <a class="dropdown-item" href="../Doc/calendario_homologado_2021.pdf" target="_blank">Calendário</a>
+                                    <a class="dropdown-item" href="https://fumec.sp.gov.br/eja/eja-profissoes" target="_blank">EJA Profissões</a>
+                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/matriz%20curricular " target="_blank">Matrizes dos cursos</a>
                                     <a class="dropdown-item" href="../QuemSomos" target="_blank">Missão,
                                         Visão e Valores</a>
-                                    <a class="dropdown-item"
-                                        href="https://www.fumec.sp.gov.br/ceprocamp/aluno/planos-ensino"
-                                        target="_blank">Planos de ensino</a>
-                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/node/634"
-                                        target="_blank">Professores Orientadores de Área - POA's</a>
-                                    <a class="dropdown-item" href="../Doc/projeto_pedagogico_2018-2021.pdf"
-                                        target="_blank">Projeto Pedagógico</a>
-                                    <a class="dropdown-item" href="../Doc/regimento_escolar_site_-_com_sumario.pdf"
-                                        target="_blank">Regimento escolar</a>
+                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/ceprocamp/aluno/planos-ensino" target="_blank">Planos de ensino</a>
+                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/node/634" target="_blank">Professores Orientadores de Área - POA's</a>
+                                    <a class="dropdown-item" href="../Doc/projeto_pedagogico_2018-2021.pdf" target="_blank">Projeto Pedagógico</a>
+                                    <a class="dropdown-item" href="../Doc/regimento_escolar_site_-_com_sumario.pdf" target="_blank">Regimento escolar</a>
 
                                 </div>
                             </li>
@@ -114,21 +113,15 @@
                     <li class="nav-item">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
-                                    id="navDrop">Aluno</a>
+                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="navDrop">Aluno</a>
 
                                 <div class="dropdown-menu">
 
-                                    <a class="dropdown-item"
-                                        href="https://www.fumec.sp.gov.br/ceprocamp/acesso-plataforma-google-class"
-                                        target="_blank">Acesso à Plataforma Google Class</a>
-                                    <a class="dropdown-item" href="../Doc/HORARIOS DE AULAS.pdf" target="_blank">Horarios
+                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/ceprocamp/acesso-plataforma-google-class" target="_blank">Acesso à Plataforma Google Class</a>
+                                    <a class="dropdown-item" href="Doc/HORARIOS DE AULAS.pdf" target="_blank">Horarios
                                         de aula</a>
-                                    <a class="dropdown-item"
-                                        href="https://www.fumec.sp.gov.br/sites/www.fumec.sp.gov.br/files/manual_do_aluno_reformulado_2021_0.pdf"
-                                        target="_blank">Manual do Aluno</a>
-                                    <a class="dropdown-item" href="../Doc/Auxilio transporte.pdf"
-                                        target="_blank">Regulamento - Auxílio Transporte</a>
+                                    <a class="dropdown-item" href="https://www.fumec.sp.gov.br/sites/www.fumec.sp.gov.br/files/manual_do_aluno_reformulado_2021_0.pdf" target="_blank">Manual do Aluno</a>
+                                    <a class="dropdown-item" href="../Doc/Auxilio transporte.pdf" target="_blank">Regulamento - Auxílio Transporte</a>
 
                                 </div>
                             </li>
@@ -159,8 +152,7 @@
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="https://www.facebook.com/fumec.cepro/">Facebook</a>
                                     <a class="dropdown-item" href="https://www.instagram.com/fumec.cepro/">Instagram</a>
-                                    <a class="dropdown-item"
-                                        href="https://www.youtube.com/channel/UC1He1mXyJyfIkIh2RvG7A7Q">Youtube</a>
+                                    <a class="dropdown-item" href="https://www.youtube.com/channel/UC1He1mXyJyfIkIh2RvG7A7Q">Youtube</a>
                                 </div>
                             </li>
                         </ul>
@@ -173,9 +165,8 @@
                         <a class="nav-link" href="https://www.fumec.sp.gov.br/fale-conosco" target="_blank">Fale
                             Conosco</a>
                     </li>
-
                 </ul>
             </div>
-        </div>
     </nav>
+
     <!-- Fim do Menu -->
