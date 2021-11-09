948cbdfa87b972c6b9a102244972d6017850bd8f
GuilhermeSalles <guibaltazarvs@gmail.com>
2021-11-09 13:50:20 -0300

    Pagina da SIPAT criada, alguns erros no css

---
+++ Trabalho_Ceprocamp/sipatceprocamp.php
@@ -0,0 +1,167 @@
+<?php include 'header-menu.php' ?>
+<style>
+    p {
+        color: rgb(75, 75, 75);
+    }
+
+    mark {
+        background-color: rgb(255 59 0 / 45%);
+    }
+</style>
+<br>
+<br>
+
+<div class="container">
+    <h3 class="bold">18ª SIPAT CEPROCAMP</h3>
+    <hr class="hr3">
+
+    <div class="text-justify">
+        <p>O CEPROCAMP, juntamente com a CIPA, Eixo de Segurança do Trabalho e apoio do Núcleo de Interação Tecnológica tem a satisfação de convidá-los para a 18ª Semana Interna de Prevenção de Acidentes do Trabalho - SIPAT que ocorrerá entre os dias 8 a 12 de novembro de 2021 em um formato totalmente virtual por meio de transmissão pelo Canal da FUMEC (TVFUMEC)</p>
+        <hr class="hr3">
+        <p><mark>Não</mark> haverá limitação de participantes.</p>
+
+
+        <p>A participação de todos é fundamental para realizarmos um belo evento!</p>
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-10 col-md-12 col-xs-12">
+
+            <table class="table table-hover">
+                <tbody>
+                    <tr>
+                        <th class="ml-0" scope="col"><a href="https://www.even3.com.br/ceprosipat2021/" class="linkNIT"> <img src="img/icones geral/link-2.svg" alt="Local"> ACESSO AO SITE DO EVENTO</a></th>
+                    </tr>
+                    <tr>
+
+                        <th class="ml-0" scope="col"><a href="https://www.youtube.com/channel/UC1He1mXyJyfIkIh2RvG7A7Q" class="linkNIT"> <img src="img/icones geral/link-2.svg" alt="Local"> ACESSO AO CANAL TVFUMEC</a></th>
+                    </tr>
+                    <tr>
+                        <th class="ml-0" scope="col"><a href="doc/sipat.pdf" class="linkNIT"> <img src="img/icones geral/link-2.svg" alt="Local"> LINK DE ACESSO AS ATIVIDADES (PDF INTERATIVO)</a></th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="text-center">
+        <h4 class="bold">PROGRAMAÇÃO:</h4>
+        <hr class="hr3">
+    </div>
+    <div class="row">
+        <div class="col-sm-10 col-md-12 col-xs-12">
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th colspan="4" class="text-center bold">10 de novembro de 2021</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 08:00 – 09:40</td>
+                        <td>Infecções Sexualmente Transmissíveis – AIDS</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 13:30 – 15:10</td>
+                        <td>Convivência e Segurança</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 15:30 – 17:10</td>
+                        <td>Assédio no ambiente de trabalho</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 18:50 – 20:30</td>
+                        <td>Introdução aos Sistemas de Ancoragem e Linha de Vida</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 20:40 – 22:20</td>
+                        <td>Combate ao incêndio: combustíveis, inflamáveis e produtos químicos perigosos</td>
+                    </tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-10 col-md-12 col-xs-12">
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th colspan="4" class="text-center bold">11 de novembro de 2021</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 10:00 – 11:40</td>
+                        <td>Treinamento de Primeiros Socorros</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 13:30 – 15:10</td>
+                        <td>Prevenção e combate a Incêndio</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 15:30 – 17:10</td>
+                        <td>Efeito dominó em acidentes no trabalho</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 18:50 – 20:30</td>
+                        <td>Efeito dominó em acidentes no trabalho</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 20:40 – 22:20</td>
+                        <td>Treinamento de segurança e saúde ocupacional em tempos de pandemia</td>
+                    </tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-10 col-md-12 col-xs-12">
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th colspan="4" class="text-center bold">12 de novembro de 2021</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 10:00 – 11:40</td>
+                        <td>Segurança do Profissional de Saúde em Época de Pandemia</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 13:30 – 15:10</td>
+                        <td>Direito trabalhista após a Reforma</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 15:30 – 17:10</td>
+                        <td>Treinamento de Primeiros Socorros</td>
+                    </tr>
+                    <tr>
+                        <td><img src="img/icones geral/calendar.svg"> 18:50 – 20:30</td>
+                        <td>Infecções Sexualmente Transmissíveis</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</div>
+
+<br>
+<br>
+<?php include 'footer.php' ?>
